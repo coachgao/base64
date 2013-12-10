7f8c9beac70e8f55072b0311f4e10d1d5ec53aba
coachgao <coachgao1986@gmail.com>
2013-12-10 23:46:23 +0800

    add decode base64(only for ascii)

--- encode.py
+++ encode.py
@@ -34,5 +34,56 @@ def encode(src):
 
 	return output
 
+def decode(src):
+
+	#ZWFzdXJlLg==
+	dict = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9, 'K':10, \
+			'L':11, 'M':12, 'N':13, 'O':14, 'P':15, 'Q':16, 'R':17, 'S':18, 'T':19, 'U':20, \
+			'V':21, 'W':22, 'X':23, 'Y':24, 'Z':25, 'a':26, 'b':27, 'c':28, 'd':29, 'e':30, \
+			'f':31, 'g':32, 'h':33, 'i':34, 'j':35, 'k':36, 'l':37, 'm':38, 'n':39, 'o':40, \
+			'p':41, 'q':42, 'r':43, 's':44, 't':45, 'u':46, 'v':47, 'w':48, 'x':49, 'y':50, \
+			'z':51, '0':52, '1':53, '2':54, '3':55, '4':56, '5':57, '6':58, '7':59, '8':60, \
+			'9':61, '+':62, '/':63}
+	
+	paddingZero = {1:'0', 2:'00', 3:'000', 4:'0000', 5:'00000', 6:'000000'}
+	delPaddingTail = {2:4, 1:2}
+	
+	n = 0
+	if src[len(src) - 1] == '=':
+		n += 1
+		if src[len(src) - 2] == '=':
+			n += 1
+	
+	str = ''
+	temp = src[:len(src) - n]
+	for c in temp:
+		value = (bin(dict[c]))[2:]
+		c = 6 - len(value)
+		if c != 0:
+			value =  paddingZero[c] + value
+		str += value
+	str = str[:len(str) - delPaddingTail[n]]
+
+	output = ''
+	for i in range(8, len(str) + 1, 8):
+		output += chr(int(str[i - 8:i], 2))
+	
+	return output
+
 if __name__ == "__main__":
 	print(encode("easure."))
+	print(decode("ZWFzdXJlLg=="))
+
+	#str = encode("你好吗！")
+	#print(str)
+	#print(decode(str))
+
+
+
+
+
+
+
+
+
+
