6d6b913876c2b7ea44924995871a2927e4f90853
coachgao <coachgao1986@gmail.com>
2013-12-12 20:23:26 +0800

    rename the source file

---
+++ base64.py
@@ -0,0 +1,61 @@
+
+
+code = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+def encodeBase64(src, encode="utf-8"):
+
+	global code
+
+	paddingTail = {0:'', 8:'0000', 16:'00'}
+	paddingEqual = {0:'', 8:'==', 16:'='}
+
+	src = src.encode(encode)
+	sin = ''
+	for c in src:
+		sin += bin(c)[2:].zfill(8)
+	n = len(sin) % 24
+	sin += paddingTail[n]
+
+	output = ''
+	for i in range(6, len(sin) + 1, 6):
+		output += code[int(sin[i - 6:i], 2)]
+	output += paddingEqual[n]
+
+	return output
+
+
+def decodeBase64(src, encode="utf-8"):
+
+	global code
+	delPaddingTail = {0:0, 2:4, 1:2}
+
+	value = ''
+	n = src.count('=')
+	sin = src[:len(src) - n]
+	for c in sin:
+		value += bin(code.find(c))[2:].zfill(6)
+	value = value[:len(value) - delPaddingTail[n]]
+
+	middle = []
+	for i in range(8, len(value) + 1, 8):
+		middle.append(int(value[i-8:i], 2))
+	output = bytes(middle).decode(encoding=encode)
+
+	return output
+
+	
+if __name__ == "__main__":
+
+	str = encodeBase64("base64", "ascii")
+	print(str)
+	print(decodeBase64(str, "ascii"))
+
+
+
+
+
+
+
+
+
+
